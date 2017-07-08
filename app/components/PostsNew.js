144a7b84a969158371c2d6ab281c0ce890412dc9
sebtn <sdebedout@gmail.com>
2017-07-08 16:21:52 -0400

    Placeholder is now showing

--- app/components/PostsNew.js
+++ app/components/PostsNew.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Field, reduxForm} from 'redux-form'
+import {Link} from 'react-router'
 
 import {createPost} from '../actions/index'
 
@@ -11,31 +12,40 @@ class PostsNew extends Component {
   render() {
     const { handleSubmit, pristine, reset, submitting } = this.props      
     return (
-      <div className="form-container">
-        <form onSubmit={ handleSubmit(createPost) }>
-          <div >
-            <label htmlFor="title">title</label>
-            <Field
-              name="title"                  
-              component={renderInput}           
-              type="text"/>                     
-          </div>
-          <div>
-            <label htmlFor="categories">categories</label>
-            <Field
-              name="categories"                   
-              component={renderInput}           
-              type="text"/>                 
-          </div>
-          <div >
-            <label htmlFor="content">content</label>
-            <Field
-              name="content"                   
-              component={renderTextArea}          
-              type="text"/>                
-          </div>
-          <button type="submit" className="btn btn-primary" disabled={submitting}>Submit</button>
-        </form>
+      <div>
+        <h1>Add a blog post</h1>
+        <div className="form-container">
+          <form onSubmit={ handleSubmit(createPost) }>
+            <div >
+              <label>Title</label>
+              <Field
+                name="title"                  
+                placeholder="Type a title for the post"
+                component={renderInput}           
+                type="text"
+                />                     
+            </div>
+            <div>
+              <label>Categories</label>
+              <Field
+                name="categories"                   
+                component={renderInput}           
+                type="text"
+                placeholder="Enter categories for the post"
+                />     
+            </div>
+            <div >
+              <label>Content</label>
+              <Field
+                name="content"                   
+                component={renderTextArea}          
+                type="text"
+                placeholder="Start typing here"/>                
+            </div>
+            <button type="submit" className="btn btn-primary" disabled={submitting}>Submit</button>
+            <Link to="/posts" className="btn btn-danger">Cancel</Link>
+          </form>
+        </div>
       </div>
     )
   }
@@ -45,44 +55,46 @@ class PostsNew extends Component {
 // Define stateless component to render input and errors
 
 const renderInput  = ({
+  placeholder,
   input,
   label,
   type,
-  meta: { touched, error, warning }
+  meta: { touched, error }
 }) => (
   <div>
     <label>{label}</label>
     <div>
       { touched &&
         (( error &&      
-          <div className="alert alert-warning" role="alert">
+          <div className="alert alert-danger" role="alert">
             <strong><span>{error}</span></strong> Better check yourself, you're not looking too good.
           </div> 
-     )  ||
-        (warning && <span>{warning}</span>)) }
+         ))
+      }
     </div>
-    <input {...input} placeholder={label} type={type} />
+    <input {...input} placeholder={placeholder} type={type} />
   </div>
 )
 
 const renderTextArea = ({
+  placeholder,
   input,
   label,
   type,
-  meta: { touched, error, warning }
+  meta: { touched, error}
 }) => (
   <div>
     <label>{label}</label>
     <div>
       { touched &&
         ((error &&  
-          <div className="alert alert-warning" role="alert">
+          <div className="alert alert-danger" role="alert">
             <strong><span>{error}</span></strong> Better check yourself, you're not looking too good.
           </div> 
-        ) ||
-        (warning && <span>{warning}</span>) ) }
+        ))
+      }
     </div>
-    <textarea {...input} placeholder={label} type={type}  rows= '7' />
+    <textarea {...input} placeholder={placeholder} type={type}  rows= '7' />
   </div>
 )    
 
@@ -90,13 +102,13 @@ const renderTextArea = ({
   const validate = values => {
     const errors = {}
     if (!values.title) {
-      errors.title = 'Required Title'
+      errors.title = 'Required title Field'
     }
     if (!values.categories) { 
-      errors.categories = 'Required categories' 
+      errors.categories = 'Required categories Field' 
     }     
     if (!values.content) { 
-      errors.content = 'Required content' 
+      errors.content = 'Required content Field' 
     } 
     return errors
   }
